591d2fa6a255afe6a5bbb4e0215f05b711511fdf
minojini2112 <profession2112@gmail.com>
2024-09-20 14:52:11 +0530

    added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
       </div>
       <div className="hidden mx-auto 2xl:container" id="headermd">
       <div className="mx-auto w-[90%] grid grid-cols-1 ">
-        <div className="flex justify-end">
+        <div className="flex justify-end ">
           <button>
             <span className="material-symbols-outlined" onClick={
               ()=>{
@@ -60,7 +60,7 @@ const Header = () => {
           </button>
         </div>
         <div className="mt-5 space-y-5">
-          <div className="flex justify-between">
+          <div className="flex justify-between border-b border-gray-500">
             <div>For Brands</div>
             <div>
               <button>
@@ -68,7 +68,7 @@ const Header = () => {
               </button>
             </div>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between border-b border-gray-500">
             <div>For Agencies</div>
             <div >
               <button>
@@ -76,7 +76,7 @@ const Header = () => {
               </button>
             </div>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between border-b border-gray-500">
             <div>Product</div>
             <div >
               <button>
@@ -86,7 +86,7 @@ const Header = () => {
               </button>
             </div>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between border-b border-gray-500">
             <div>Resources</div>
             <div >
               <button>
@@ -96,7 +96,7 @@ const Header = () => {
               </button>
             </div>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between border-b border-gray-500">
             <div>Pricing</div>
             <div>
               <button>
@@ -105,7 +105,7 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center mt-20">
+        <div className="flex items-center justify-center mt-40">
           
           <button className="flex items-center justify-center p-3 px-10 font-bold text-white bg-orange-500 border border-red-500 rounded-xl hover:shadow-orange-100 hover:shadow-xl">
             Request a Demo
